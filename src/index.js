abbce1f0f2b409c8bb232c92b3714efc8315c997
Bruce Chamoff <info@hotwebideas.net>
2018-04-16 19:41:00 -0400

    Added Marquee component and started styling the CSS.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Marquee extends Component {
+	render() {
+		return (
+			<section className="music-marquee">
+				<div className="headings">
+				{this.props.mp3artist} - {this.props.children} ({this.props.mp3genre})
+				</div>
+			</section>
+		);
+	}
+}
+
 class MP3Player extends Component {
 	render() {
 		return (
-			<h1>{this.props.title}</h1>
+			<div className="audio-container">
+				<h1>{this.props.title}</h1>
+				<Marquee mp3artist="Bruce Chamoff" mp3genre="Rock">Blood</Marquee>
+			</div>
 		);
 	}
 }
